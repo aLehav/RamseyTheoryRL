597a57e0f4f7a8bf71ab613058525e655abc0ec8
Steve Vott <svott@usc.edu>
2023-05-16 13:18:23 -0700

    feature counterp2

--- src/utils/features.py
+++ src/utils/features.py
@@ -3,6 +3,8 @@ import networkx as nx
 import itertools
 
 # Count occurences of feature: Exponential runtime
+
+
 def num_complete_subgraphs(G, i):
     return len(list(nx.enumerate_all_cliques(G))) if i > 2 else len([clq for clq in nx.enumerate_all_cliques(G) if len(clq) == i])
 
@@ -47,6 +49,77 @@ def num_bipartite_subgraphs(G, m, n):
                 count += 1
     return count
 
+# Count number of occurences of features in table 2 of paper for every 4 subgraph
+
+
+def count_subgraph_structures(G):
+    # Define the structures to look for
+    structures = {
+        "K_2": nx.complete_graph(2),
+        "P_3": nx.path_graph(3),
+        "K_{1,3}": nx.complete_bipartite_graph(1, 3),
+        "P_4": nx.path_graph(4),
+        "K_3": nx.complete_graph(3),
+        "C_4": nx.cycle_graph(4),
+        "K_4": nx.complete_graph(4),
+    }
+    # Add K_3+e
+    K_3_e = nx.complete_graph(3)
+    K_3_e.add_edge(2, 3)
+    structures["K_3+e"] = K_3_e
+
+    # Add K_4-e
+    K_4_e = nx.complete_graph(4)
+    K_4_e.remove_edge(0, 1)
+    structures["K_4-e"] = K_4_e
+
+    # Convert each structure graph to a canonical hash for comparison
+    for name, graph in structures.items():
+        structures[name] = nx.weisfeiler_lehman_graph_hash(graph)
+
+    # Initialize a counter for each structure
+    counters = {name: 0 for name in structures}
+
+    # Enumerate all 4-node subgraphs
+    for nodes in itertools.combinations(G.nodes, 4):
+        subgraph = nx.subgraph(G, nodes)
+
+        # Compute the canonical hash of the subgraph
+        hash = nx.weisfeiler_lehman_graph_hash(subgraph)
+
+        # If the hash matches any of the structures, increment the corresponding counter
+        for name, structure_hash in structures.items():
+            match name:
+                case "K_2":
+                    counters["K_2"] += subgraph.number_of_edges() == 1
+                case "P_3":
+                    for subnodes in itertools.combinations(nodes, 3):
+                        sub_subgraph = nx.subgraph(G, subnodes)
+                        sub_hash = nx.weisfeiler_lehman_graph_hash(
+                            sub_subgraph)
+                        if sub_hash == structures["P_3"] and subgraph.number_of_edges() == 2:
+                            counters["P_3"] += 1
+                            break
+                case "K_3":
+                    for subnodes in itertools.combinations(nodes, 3):
+                        sub_subgraph = nx.subgraph(G, subnodes)
+                        sub_hash = nx.weisfeiler_lehman_graph_hash(
+                            sub_subgraph)
+                        if sub_hash == structures["K_3"] and subgraph.number_of_edges() == 3:
+                            counters["K_3"] += 1
+                            break
+                case "K_3+e":
+                    for subnodes in itertools.combinations(nodes, 3):
+                        sub_subgraph = nx.subgraph(G, subnodes)
+                        sub_hash = nx.weisfeiler_lehman_graph_hash(
+                            sub_subgraph)
+                        if sub_hash == structures["K_3"] and subgraph.number_of_edges() == 4:
+                            counters["K_3+e"] += 1
+                            break
+                case _:
+                    counters[name] += (hash == structure_hash)
+
+    return counters
 
 # Update graph given an increase/decrease in a particular feature
 
