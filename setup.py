a0f48cc0e7506f2b65ef69cff52b0e23fb3cf57e
Steve Vott <svott@usc.edu>
2023-07-02 20:12:22 -0700

    packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RamseyTheoryRL",
+    version="0.1",
+    packages=find_packages(),
+    author="Adam Lehavi, Steve Vott",
+    author_email="svott03@gmail.com, alehavi@usc.edu",
+    description="Ramsey Number Explorer",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    url="https://github.com/aLehav/RamseyTheoryRL",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
